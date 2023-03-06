50c06c6f402e8f8cc88dd1fda3a499db6d3525aa
Deepthi <deepthireddyg@gmail.com>
2023-03-06 11:17:28 -0500

    code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        int nums[]=new int[4];
+         nums[0]=2;
+         nums[1]=4;
+         nums[2]=8;
+         nums[3]=8;
+
+         for(int i=0;i<=4;i++){
+             System.out.println(nums[i]);
+         }
         System.out.println("Hello world!");
     }
 }
